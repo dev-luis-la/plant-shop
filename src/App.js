34ab025875029ff102bfd29deb043b1ad2000a2d
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-02-22 13:55:31 -0500

    working on header nav

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from "react";
 import './App.css'
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.comp.jsx";
-import { Route } from "react-router-dom";
+import Header from "./components/header/header";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div >
-    <Route exact path='/' component={HomePage}/>
-    <Route  path='/shop' component={ShopPage}/>
+    <Header/>
+    <switch>
+       <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
+    </switch>
+   
     </div>
   );
 }
