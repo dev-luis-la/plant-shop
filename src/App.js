8e3d78d1263fcf525239c8b3f2a68c6893d5ce50
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-02-24 02:00:04 -0500

    tweaks to sigin function

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import ShopPage from "./pages/shoppage/shop.comp.jsx";
 import Header from "./components/header/header";
 import Form from  "./pages/signin-out/signin-out"
 import { Route, Switch } from "react-router-dom";
+import {auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div >
-    <Header/>
-    <Switch>
-       <Route exact path='/' component={HomePage}/>
-        <Route  path='/shop' component={ShopPage}/>
-        <Route  path='/signin' component={Form}/>
-        
-    </Switch>
-   
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubcribeFromAuth = null
+
+  componentDidMount(){
+   this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    });
+  }
+  componentWillUnmount(){
+    this.unsubcribeFromAuth();
+  }
+  render(){
+    return (
+      <div >
+      <Header/>
+      <Switch>
+         <Route exact path='/' component={HomePage}/>
+          <Route  path='/shop' component={ShopPage}/>
+          <Route  path='/signin' component={Form}/>
+          
+      </Switch>
+     
+      </div>
+    );
+  }
 }
 
 export default App;
